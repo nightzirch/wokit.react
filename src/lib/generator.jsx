4146c088f64018ca3d82ada0a96eee6b14ef6d66
Christian Grimsgaard <christian.grimsgaard@knowit.no>
2017-11-16 21:10:24 +0100

    More generated stuff

--- src/lib/generator.jsx
+++ src/lib/generator.jsx
@@ -17,16 +17,11 @@ export default {
   },
 
   getBreadText: () => {
-    let alternatives = [
-      'Poop in the plant pot chew on cable cats go for world domination so lick arm hair open the door, let me out, let me out, let me-out, let me-aow, let meaow, meaow!',
-      'Stick butt in face bleghbleghvomit my furball really tie the room together yet destroy couch as revenge sit by the fire hiss and stare at nothing then run.',
-      'Scratch the box and sometimes switches in french and say "miaou" just because well why not.',
-      'Chase after silly colored fish toys around the house vommit food and eat it again so lick the curtain just to be annoying so favor packaging over toy.',
-      'Massacre a bird in the living room and then look like the cutest and most innocent animal on the planet howl.',
-      'Your pillow is now my pet bed flop over gnaw the corn cob or show belly.'
-    ];
+    return r(d.breadText);
+  },
 
-    return r(alternatives);
+  getParagraph: () => {
+    return r(d.paragraph);
   }
 }
 
