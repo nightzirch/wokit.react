6002a127aced45b1b56d7ece15a5535348c7b158
Christian Grimsgaard <christian.grimsgaard@knowit.no>
2017-11-16 20:11:04 +0100

    Moved generator

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Generator from '../data/generator';
 
 import {
   Container
@@ -8,12 +7,7 @@ import {
 class FrontPage extends Component {
   render() {
     return (
-      <div className="page-container front-page">
-        <Container>
-          <h1>{Generator.getRecipeName()}</h1>
-          <p>{Generator.getIngredient()}</p>
-          <p>{Generator.getBreadText()}</p>
-        </Container>
+      <div className="page-container front-page">>
       </div>
     );
   }
