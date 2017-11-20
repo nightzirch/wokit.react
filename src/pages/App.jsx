01656673649293547bebb26a77da388c81bf6d93
Christian Grimsgaard <christian.grimsgaard@knowit.no>
2017-11-20 08:11:17 +0100

    Removed null pointers

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 
-import {
-  Footer,
-  Header
-} from '../components';
-
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Header />
-        {this.props.children}
-        <Footer />        
+        {this.props.children}      
       </div>
     );
   }
